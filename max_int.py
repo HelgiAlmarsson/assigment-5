3611918c51043fd52e4cc497f6042d50e91a6361
ulfars20 <ulfars20@ru.is>
2020-09-03 09:45:15 +0000

    Initial commit

---
+++ max_int.py
@@ -0,0 +1,7 @@
+# Prompt for a number
+# Check if less than 0
+    # Break if less and print the number from before
+    # Program stops
+# Check if new number is larger than the one before
+# If larger then change to that number
+# Go back to prompting
