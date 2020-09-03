32d755ab92d000502e2aab0a148a156345369341
ulfars20 <ulfars20@ru.is>
2020-09-03 10:23:50 +0000

    Algorithm written for sequence 2

--- sequence.py
+++ sequence.py
@@ -1,10 +1,19 @@
-# Last three numbers generate the next
-# 
 # Prompts for number
+n = int(input("Enter the length of the sequence: ")) # Do not change this line
+num0 = 0
+num1 = 1
+num2 = 2
+
+# Print 1, 2
+print(num1)
+print(num2)
 # Make a for loop
-# Print 0, 1, 2
-# Add the numbers together
-# num0 = num1
-# num1 = num2
-# num2 = new_number
-# When loop finished print result
+for i in range(0, n - 2):
+    # Add the numbers together
+    new_number = num0 + num1 + num2
+    # Update numbers
+    num0 = num1
+    num1 = num2
+    num2 = new_number
+    # Print number
+    print(new_number)
