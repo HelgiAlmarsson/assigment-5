f0f6dc02a0e347c521cc13aa90cb516a4c5758c0
ulfars20 <ulfars20@ru.is>
2020-09-03 10:05:54 +0000

    Algorithm written for sequence

---
+++ sequence.py
@@ -0,0 +1,10 @@
+# Last three numbers generate the next
+# 
+# Prompts for number
+# Make a for loop
+# Print 0, 1, 2
+# Add the numbers together
+# num0 = num1
+# num1 = num2
+# num2 = new_number
+# When loop finished print result
